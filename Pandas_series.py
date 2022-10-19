c8aca819591141eccc384069e2f9c0ab8cf974d3
VForti <99172698+VForti@users.noreply.github.com>
2022-10-19 19:31:02 +0300

    Update Pandas_series.py

--- Pandas_series.py
+++ Pandas_series.py
@@ -7,12 +7,19 @@ import pandas as pd
 # in our case it is int64, because we passed integer values.
 
 my_series = pd.Series([3, 1, 6, 2, 9, 4])
-print(my_series)
+print(my_series) # 0    3
+                 # 1    1
+                 # 2    6
+                 # 3    2
+                 # 4    9
+                 # 5    4
+                 # dtype: int64
+
 
 # The Series object has attributes through which you can get a list of elements and indices,
 # these are values and index, respectively.
 
 #index
-print(my_series.index)
+print(my_series.index) # RangeIndex(start=0, stop=6, step=1)
 #values
-print(my_series.values)
+print(my_series.values) # [3, 1, 6, 2, 9, 4]
